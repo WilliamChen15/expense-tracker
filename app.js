cc30e217d71cfd81a78b77c1fccd329da313d483
WilliamChen15 <turelight13@gmail.com>
2023-01-10 13:29:01 +0800

    feat: session

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
 // 主程式入口
-
+// session 還沒裝
 const express = require('express')
-const app = express()
+
+const exphbs = require('express-handlebars')
+
+const bodyParser = require('body-parser')
+
+const session = require('express-session')
+
+const methodOverride = require('method-override')
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const port = process.env.PORT
-const exphbs = require('express-handlebars')
+
 const usePassport = require('./config/passport')
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
+
+const app = express()
 const routes = require('./routes')
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(bodyParser.urlencoded({ extended: true }))
+
 app.use(methodOverride('_method'))
+
 app.use(express.static('public'))
+
 usePassport(app)
+
 require('./config/mongoose')
+
 app.use(routes)
 
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 // 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
+
 const home = require('./modules/home')
 const expense = require('./modules/expense')
 const users = require('./modules/users')
-const { authenticator } = require('./modules/auth')
-
-router.use('/', authenticator, home) // 登入後才能用
-router.use('/expense', authenticator, expense)
+// const auth = require('./modules/auth')
+const { authenticator } = require('../middleware/auth')
+router.use('/users', users) // 順序很重要
 // router.use('/auth', auth) // FB登入
-router.use('/users', users)
+router.use('/expense', authenticator, expense)
+router.use('/', authenticator, home) // 登入後才能用
 // 匯出路由器
+
 module.exports = router
